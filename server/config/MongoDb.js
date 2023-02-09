624266814962d78864a0fe7b29477552fa5fc96f
Jian Lu <jason.lu1932@gmail.com>
2023-02-09 16:53:55 -0500

    finished OAuth

--- server/config/MongoDb.js
+++ server/config/MongoDb.js
@@ -6,27 +6,27 @@ const uri = `mongodb+srv://${keys.MongoUser}:${keys.MongoPassword}@cluster0.axjj
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  dbName: 'Stonkz'
+  dbName: 'Aionic'
 })
   .then(() => console.log('Connected to Mongo DB.'))
   .catch(err => console.log(err));
 
 const AppSchema = new Schema({
-  name: {type: String, default: ''},
+  name: {type: String, required: true},
+  uid: {type: String, required: true, unique: true},
   head: {type: String, default: null},
   tail: {type: String, default: null}
 })
 
-const UserSchema = new Schema({
-  gubhibId: {type: String, required: true},
-  argo_token: {type: String, default: ''},
-  App: {type: Array, default: [AppSchema]}
-})
-
 const NodeSchema = new Schema({
-  manifest: {type: String}, //stringafy
+  manifest: {type: String}, //stringify
   prev: {type: String, default: null},
   next: {type: String, default: null}
+})
+
+const UserSchema = new Schema({
+  githubId: {type: String, required: true},
+  argo_token: {type: String, default: ''}
 })
 
 const User = new mongoose.model("User", UserSchema)
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const cookieSession = require('cookie-session');
 const express = require('express');
 const cors = require('cors');
 const passport = require('passport');
+const dbRouter = require('./routes/dbrouter')
 const app = express();
 
 //importing middleware and keys
@@ -29,30 +30,42 @@ app.use(passport.session());
 
 //github will return an access token and passport will do its magic in grabbing the data and storing the user in the database
 //check passport.js for more info
-app.get('/auth/github/callback', 
+app.get('/auth/github/callback',
   passport.authenticate('github', { failureRedirect: '/' }),
   (req, res) => {
-    res.redirect('/');
+    return res.redirect('http://localhost:5173/');
   });
 
 //sends user to github for authorization and guthub will send back a code for us to grab the github access token
-app.get('/auth/github', 
+app.get('/auth/github',
   passport.authenticate('github', { scope: ['user:email'] }));
 
 //logout session that destroy cookie and log user out from github
 app.get('/logout', (req, res) => {
   req.session = null;
   req.logout();
-  res.redirect('/');
+  return res.redirect('/');
 })
 
 app.get('/', authController.isLoggedIn, (req, res) => {
-  res.json('success');
+  return res.json('success');
 })
 
 app.get('/test', argoController.setToken, (req, res) => {
   console.log(JSON.parse(res.locals.manifest.manifests[1]))
-  res.json(res.locals.manifest);
+  return res.json(res.locals.manifest);
+})
+
+app.use('/db', dbRouter)
+
+app.use((err,req,res,next) => {
+  const error = {
+    message: 'you will never know what is wrong with me **hint** server related ',
+    status: 404,
+    log: 'if you see this text you did not setup error handling -.-;;'
+  }
+  err = Object.assign(error, err)
+  return res.status(err.status).send(err)
 })
 
 //start the server
