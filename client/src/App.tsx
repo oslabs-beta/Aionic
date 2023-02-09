62c79b7e60246bd5b50b8863fc0077abb0b76fb9
Ari Bengiyat <ari@aribengiyat.com>
2023-02-09 17:15:21 -0500

    merged dev into fecomp

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,13 @@
-import { useState } from 'react';
-import { redirect } from 'react-router';
-import Home from './components/Home';
-import Login from './components/Login';
-
-
-
-
 function App() {
-  // const  [login, setLogin]  = useState(false);
-  const handleClick = (e : React.ChangeEvent<HTMLButtonElement>): void => {
+  const handleClick = (e) => {
     e.preventDefault();
     fetch('http://localhost:3000/test')
       .then((data) => data.json())
       .then((data) => {
         console.log(data);
-      })
-      .catch((err) => console.log(err));
+    })
   }
+
   return (
     <div>
       <h1>Vite is the best</h1>
