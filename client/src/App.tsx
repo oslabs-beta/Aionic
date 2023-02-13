01e103c881b24375e3ac25e379baa22824dc24da
nathanmlui <nathanmlui@gmail.com>
2023-02-13 11:03:59 -0500

    isLoggedIn logic for App componenet

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,32 @@
+import { ChangeEvent, useState, useEffect } from 'react';
+import Home from './components/Home';
+import Login from './components/Login';
+
 function App() {
-  const handleClick = (e) => {
-    e.preventDefault();
-    fetch('http://localhost:3000/test')
-      .then((data) => data.json())
-      .then((data) => {
-        console.log(data);
-    })
-  }
+  //isLoggedIn changes after successful GitHub auth
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    //fetch the api
+    fetch('http://localhost:3000/api/checkuser')
+      .then((data: Response) => data.json())
+      .then((data: string) => {
+        //if auth succeeds, we get the username back. need to make sure we get false if it fails
+        if (data != 'failed') {
+          setIsLoggedIn(true);
+          //forward to home
+        }
+      });
+  });
 
   return (
     <div>
       <h1>Vite is the best</h1>
-      <button onClick={(e)=>handleClick(e)}>CLICK ME</button>
+      <button onClick={(e) => handleClick(e)}>CLICK ME</button>
       <a href='http://localhost:3000/auth/github'>Login Using GitHub</a>
-   </div>
-  )
+      {isLoggedIn ? <Home /> : <Login />}
+    </div>
+  );
 }
 
-export default App
+export default App;
