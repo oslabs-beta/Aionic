714efa54a2f81585a690541f5a503dd563d135c7
tk-0311 <113018868+tk-0311@users.noreply.github.com>
2023-02-13 16:56:58 -0500

    did not change anything

--- server/config/MongoDb.js
+++ server/config/MongoDb.js
@@ -11,6 +11,9 @@ mongoose.connect(uri, {
   .then(() => console.log('Connected to Mongo DB.'))
   .catch(err => console.log(err));
 
+
+
+
 const AppSchema = new Schema({
   name: {type: String, required: true},
   uid: {type: String, required: true, unique: true},
@@ -19,8 +22,10 @@ const AppSchema = new Schema({
 })
 
 const UserSchema = new Schema({
-  githubId: {type: String, required: true, unique: true},
+  gubhibId: {type: String, required: true, unique: true},
   githubToken: {type: String, default: ''}
+  // argo_token: {type: String, default: ''},
+  // app_ids: {type: Array, default: [{app_id: {type: String}}]}
 })
 
 const NodeSchema = new Schema({
@@ -29,7 +34,6 @@ const NodeSchema = new Schema({
   prev: {type: String, default: null},
   next: {type: String, default: null}
 })
-
 const ApiKeySchema = new Schema({
   api_key: {type: String, required: true},
   url: {type: String, required: true}
