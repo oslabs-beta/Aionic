71e5ce604d36d29162273421a6e4afbe50fcf7d5
tk-0311 <113018868+tk-0311@users.noreply.github.com>
2023-02-14 11:28:38 -0500

    fixed duplication error

--- server/middleware/checkupdate.js
+++ server/middleware/checkupdate.js
@@ -4,11 +4,12 @@ const axios = require('axios')
 function checkManifestUpdate(app, url, api_key) {
   this.apikey = api_key
   this.url = url
-  this.time = 3000
+  this.time = 1000
   this.checkingManifest = false;
   this.app = app
-  this.revision = undefined
   this.intervalId = undefined
+  this.revision = undefined
+
 }
 
 checkManifestUpdate.prototype.update = function() {
@@ -17,18 +18,22 @@ checkManifestUpdate.prototype.update = function() {
     this.intervalId = setInterval(async ()=>{
       try{
         console.log('Checking Manifest Update')
-        if (this.revision === undefined) {
+          if (this.revision === undefined){
           const LastNode = await Node.findOne({_id: this.app.tail})
-           this.revision = await LastNode.revision
+          const {revision} = await LastNode
+          this.revision = revision
         }
 
         const response = await axios.get(`${this.url}/api/v1/applications/${this.app.name}/manifests`, {
           headers: {
             Authorization: `Bearer ${this.apikey}`
           }})
-          const {manifests, revision} = response.data
-        if (revision !== this.revision) {
-          const newNode = addNode(undefined, {locals:{uid: this.app.uid, manifest: JSON.stringify(manifests), revision: revision}})
+          const {manifests} = response.data
+        if (this.revision !== response.data.revision) {
+          console.log('revisions:',this.revision,'second', response.data.revision)
+          const newNode = await addNode(undefined, {locals:{uid: this.app.uid, manifest: JSON.stringify(manifests), revision: response.data.revision}})
+          console.log('newNode',newNode.revision)
+          this.revision = await newNode.revision
           return newNode
         }
 
