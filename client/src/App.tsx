a7b8ca4ac6f895086faffa198f01364d3087e761
Ari Bengiyat <ari@aribengiyat.com>
2023-02-28 11:31:39 -0500

    yaml formatting done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { ChangeEvent, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router';
-import Home from './components/Home';
 import Login from './components/Login';
+import logo from './assets/logo-sm.png'
 
 //successful auth will return an object with this property
 interface User {
@@ -14,7 +14,9 @@ function App() {
   const navigate = useNavigate();
 
     return (
-      <div className=''>
+      <div className='flex flex-col items-center justify-center h-screen bg-gradient-to-r from-red-400 via-blue-200 to-orange-400 gap-4'>
+        <h1 className='text-2xl text-black font-semibold underline'>Aionic</h1>
+        <img src={logo} alt="Aionic logo" className='w-36'/>
         <a href='http://localhost:3000/auth/github' className='text-lg text-black font-semibold underline'>Login Using GitHub</a>
       </div>
     );
